3a271235b5b5bbca6587f30878c3ec24cb64b809
Robert Hysa <robert.hysa@wp.pl>
2021-03-10 21:37:44 +0100

    add mentor sugestions in initAccordion

--- src/js/script.js
+++ src/js/script.js
@@ -94,19 +94,19 @@
 
         /* prevent default action for event */
         event.preventDefault();
+
         thisProduct.element.classList.toggle('active');
 
         /* find active product (product that has active class) */
         const activeProducts = document.querySelectorAll(select.all.menuProductsActive);
-        console.log('activeProducts:', activeProducts);
+
         for (let activeProduct of activeProducts) {
-          activeProduct.classList.remove('active');
 
           /* if there is active product and it's not thisProduct.element, remove class active from it */
-          if (activeProduct.element !== thisProduct.element){
+          if (activeProduct !== thisProduct.element){
 
             /* toggle active class on thisProduct.element */
-            thisProduct.element.classList.toggle('active');
+            activeProduct.classList.remove('active');
           }
         }
       });
