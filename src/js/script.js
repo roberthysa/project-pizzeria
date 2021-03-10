235b0f423e97b2bf3dbf413d4f0df4c5ef840204
Robert Hysa <robert.hysa@wp.pl>
2021-03-10 20:04:08 +0100

    add method initAccordion

--- src/js/script.js
+++ src/js/script.js
@@ -60,6 +60,7 @@
       thisProduct.data = data;
 
       thisProduct.renderInMenu();
+      thisProduct.initAccordion();
 
       console.log('new Product:', thisProduct);
     }
@@ -79,6 +80,35 @@
 
       /* add element to menu */
       menuContainer.appendChild(thisProduct.element);
+    }
+
+    initAccordion(){
+      const thisProduct = this;
+
+      /* find the clickable trigger (the element that should react to clicking) */
+      const clickableTrigger = thisProduct.element.querySelector(select.menuProduct.clickable);
+      console.log('clickableTrigger:', clickableTrigger);
+
+      /* START: add event listener to clickable trigger on event click */
+      clickableTrigger.addEventListener('click', function(event) {
+
+        /* prevent default action for event */
+        event.preventDefault();
+
+        /* find active product (product that has active class) */
+        const activeProduct = document.querySelectorAll(classNames.menuProduct.wrapperActive);
+        console.log('activeProduct:', activeProduct);
+        if (activeProduct) {
+          activeProduct.classNames.menuProduct.wrapperActive.remove('active');
+        }
+
+        /* if there is active product and it's not thisProduct.element, remove class active from it */
+        if (activeProduct !== thisProduct.element){
+
+          /* toggle active class on thisProduct.element */
+          thisProduct.element.classList.toggle('active');
+        }
+      });
     }
   }
 
