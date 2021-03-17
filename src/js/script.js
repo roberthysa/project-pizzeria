6dedd7460d227a2b58d5f683f025ea282f4c0520
Robert Hysa <robert.hysa@wp.pl>
2021-03-17 19:55:45 +0100

    add method initActions in class Cart

--- src/js/script.js
+++ src/js/script.js
@@ -328,16 +328,27 @@
       thisCart.products = [];
 
       thisCart.getElements(element);
+      thisCart.initActions();
 
-      console.log('new Cart:', thisCart);
+      // console.log('new Cart:', thisCart);
+    }
+
+    initActions(){
+      const thisCart = this;
+
+      thisCart.dom.toggleTrigger.addEventListener('click', function(event){
+        event.preventDefault();
+
+        thisCart.dom.wrapper.classList.toggle(classNames.cart.wrapperActive);
+      });
     }
 
     getElements(element){
       const thisCart = this;
 
       thisCart.dom = {};
-
       thisCart.dom.wrapper = element;
+      thisCart.dom.toggleTrigger = thisCart.dom.wrapper.querySelector(select.cart.toggleTrigger);
     }
   }
 
