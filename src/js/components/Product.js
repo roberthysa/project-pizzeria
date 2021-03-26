98fc928bf5c2d454a11b1569f1ac30d2cda3acbc
Robert Hysa <robert.hysa@wp.pl>
2021-03-26 12:23:06 +0100

    fix bug in import paths

--- src/js/components/Product.js
+++ src/js/components/Product.js
@@ -1,6 +1,6 @@
-import {select, classNames, templates} from './settings.js';
-import utils from './utils.js';
-import AmountWidget from './components/AmountWidget.js';
+import {select, classNames, templates} from '../settings.js';
+import utils from '../utils.js';
+import AmountWidget from './AmountWidget.js';
 
 class Product{
   constructor(id, data){
@@ -169,7 +169,7 @@ class Product{
     const event = new CustomEvent('add-to-cart', {
       bubbles: true,
       detail: {
-        product: thisProduct,
+        product: thisProduct.prepareCartProduct(),
       },
     });
 
